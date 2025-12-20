27022667c0873f05ff381ca5198c914b3e40454c
Janos Wolosz <janos.wolosz@gmail.com>
2025-12-20 09:09:48 +0100

    Steps ahead

--- problems/lie_7.lean
+++ problems/lie_7.lean
@@ -13,6 +13,12 @@ lemma iSup_rootSpace_eq_top :
     H.toLieSubmodule ⊔ ⨆ α : {α : Weight K H L // α.IsNonZero}, genWeightSpace L α = ⊤ := by
   admit
 
+/--
+  PROVIDED SOLUTION::
+  Since H consists of semisimple elements, it acts diagonalisably on I, and so I has a basis of common
+  eigenvectors for the elements of ad H. As we know that each root space L α is 1-dimensional by
+  LieAlgebra.IsKilling.finrank_rootSpace_eq_one, this implies lieIdeal_eq_iSup_inf.
+-/
 lemma lieIdeal_eq_iSup_inf (I : LieIdeal K L) :
     I.toSubmodule = (I.toSubmodule ⊓ H.toLieSubmodule.toSubmodule) ⊔
       ⨆ α : {α : Weight K H L // α.IsNonZero}, I.toSubmodule ⊓ (genWeightSpace L α.1).toSubmodule := by
