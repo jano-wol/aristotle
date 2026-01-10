2835d753b3491d3bbc6459b77a97340a82876283
Janos Wolosz <janos.wolosz@gmail.com>
2026-01-10 13:29:27 +0100

    Steps ahead

--- problems/lie_15.lean
+++ problems/lie_15.lean
@@ -29,19 +29,38 @@ theorem compl_eq_killingCompl (I : LieIdeal K L) :
 theorem isSimple_of_isIrreducible (hIrr : (rootSystem H).IsIrreducible) : IsSimple K L := by
   by_contra h_not_simple
   obtain ⟨I, hI_ne_bot, hI_ne_top⟩ : ∃ I : LieIdeal K L, I ≠ ⊥ ∧ I ≠ ⊤ := by
-    sorry
+    have h_not_simple_def : ¬LieAlgebra.IsSimple K L → ∃ I : LieIdeal K L, I ≠ ⊥ ∧ I ≠ ⊤ := by
+      intro h_not_simple_def
+      simp [LieAlgebra.IsSimple] at h_not_simple_def;
+      contrapose! h_not_simple_def;
+      constructor;
+      · exact fun I => Classical.or_iff_not_imp_left.2 fun h => h_not_simple_def I h;
+      · cases subsingleton_or_nontrivial L <;> simp_all +decide [ LieAlgebra.IsKilling ];
+        · have := hIrr.1; simp_all +decide [ LieAlgebra.IsKilling.rootSystem ] ;
+          exact False.elim ( this.exists_pair_ne.elim fun x hx => hx.elim fun y hy => hy <| Subsingleton.elim _ _ );
+        · intro h_abelian
+          have h_center : LieAlgebra.center K L = ⊤ := by
+            exact?
+          have h_center_eq_bot : LieAlgebra.center K L = ⊥ := by
+            exact?
+          exact absurd h_center_eq_bot (by
+          simp +decide [ h_center ];
+          exact absurd ( h_center_eq_bot ▸ h_center ) ( by simp +decide [ LieSubmodule.eq_bot_iff ] ));
+    exact h_not_simple_def h_not_simple
   let J := I.killingCompl
   obtain ⟨Φ₁, hΦ₁⟩ := exists_rootSet_lieIdeal_eq (H := H) I
   obtain ⟨Φ₂, hΦ₂⟩ := exists_rootSet_lieIdeal_eq (H := H) J
 
   have s1 : H.toSubmodule = (I.toSubmodule ⊓ H.toSubmodule) ⊔ (J.toSubmodule ⊓ H.toSubmodule) := by
     sorry
+/-
   have s2 : Φ₁ ∩ Φ₂ = ∅ := by
     sorry
   have s3 : Φ₁ ∪ Φ₂ = Set.univ := by
     sorry
   have s4 : Φ₁ ≠ ∅ := by
     sorry
+--/
   admit
 
 end LieAlgebra.IsKilling
